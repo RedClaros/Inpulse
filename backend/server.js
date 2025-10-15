23041093f90e4ef7683e16ec05f70386153d0b5f
Red Claros <levidredc@gmail.com>
2025-10-16 03:17:58 +0800

    Fix server error

--- backend/server.js
+++ backend/server.js
@@ -38,6 +38,7 @@ app.use(cors({
 app.use(express.json());
 app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false }));
 app.use(passport.initialize());
+app.use(passport.session());
 app.use('/uploads', express.static('uploads'));
 
 // 5. Configure Passport
